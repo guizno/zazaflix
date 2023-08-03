3fb115f95b8bab08143f22ccbc64c98da2f0e04d
Eduardo <eduardo.dametto@etec.sp.gov.br>
2023-08-03 12:28:51 -0300

    Ligando ao banco de dados

--- ZazaFlix/Repositories/GenreRepository.cs
+++ ZazaFlix/Repositories/GenreRepository.cs
@@ -1,33 +1,109 @@
+using System.Data;
 using ZazaFlix.Interfaces;
 using ZazaFlix.Models;
+using MySql.Data.MySqlClient;
 
 namespace ZazaFlix.Repositories;
+
 public class GenreRepository : IGenreRepository
 {
-    string connectionString = "server=localhost;port=3306;database=zazaflixdb;uid=root;pwd=''";
+    readonly string connectionString = "server=localhost;port=3306;database=GalloFlixdb;uid=root;pwd=''";
 
     public void Create(Genre model)
     {
-        throw new NotImplementedException();
+        MySqlConnection connection = new(connectionString);
+        string sql = "insert into Genre(Name) values (@Name)";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Name", model.Name);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 
     public void Delete(int? id)
     {
-        throw new NotImplementedException();
+        MySqlConnection connection = new(connectionString);
+        string sql = "delete from Genre where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", id);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 
     public List<Genre> ReadAll()
     {
-        throw new NotImplementedException();
+        MySqlConnection connection = new(connectionString);
+        string sql = "select * from Genre";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        
+        List<Genre> genres = new();
+        connection.Open();
+        MySqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Genre genre = new()
+            {
+                Id = reader.GetByte("id"),
+                Name = reader.GetString("name")
+            };
+            genres.Add(genre);
+        }
+        connection.Close();
+        return genres;
     }
 
     public Genre ReadById(int? id)
     {
-        throw new NotImplementedException();
+        MySqlConnection connection = new(connectionString);
+        string sql = "select * from Genre where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", id);
+        
+        connection.Open();
+        MySqlDataReader reader = command.ExecuteReader();
+        reader.Read();
+        if (reader.HasRows)
+        {
+            Genre genre = new()
+            {
+                Id = reader.GetByte("id"),
+                Name = reader.GetString("name")
+            };
+            connection.Close();
+            return genre;
+        }
+        connection.Close();
+        return null;
     }
 
     public void Update(Genre model)
     {
-        throw new NotImplementedException();
+        MySqlConnection connection = new(connectionString);
+        string sql = "update Genre set Name = @Name where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", model.Id);
+        command.Parameters.AddWithValue("@Name", model.Name);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
     }
 }
---
+++ ZazaFlix/Repositories/MovieRepository.cs
@@ -0,0 +1,144 @@
+using System.Data;
+using ZazaFlix.Interfaces;
+using ZazaFlix.Models;
+using MySql.Data.MySqlClient;
+
+namespace ZazaFlix.Repositories;
+
+public class MovieRepository : IMovieRepository
+{
+    readonly string connectionString = "server=localhost;port=3306;database=GalloFlixdb;uid=root;pwd=''";
+
+    public void Create(Movie model)
+    {
+        MySqlConnection connection = new(connectionString);
+        string sql = "insert into Movie(Title, OriginalTitle, Synopsis, MovieYear, Duration, AgeRating, Image) "
+              + "values (@Title, @OriginalTitle, @Synopsis, @MovieYear, @Duration, @AgeRating, @Image)";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Title", model.Title);
+        command.Parameters.AddWithValue("@OriginalTitle", model.OriginalTitle);
+        command.Parameters.AddWithValue("@Synopsis", model.Synopsis);
+        command.Parameters.AddWithValue("@MovieYear", model.MovieYear);
+        command.Parameters.AddWithValue("@Duration", model.Duration);
+        command.Parameters.AddWithValue("@AgeRating", model.AgeRating);
+        command.Parameters.AddWithValue("@Image", model.Image);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    public void Delete(int? id)
+    {
+        MySqlConnection connection = new(connectionString);
+        string sql = "delete from Movie where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", id);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    public List<Movie> ReadAll()
+    {
+        MySqlConnection connection = new(connectionString);
+        string sql = "select * from Movie";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        
+        List<Movie> movies = new();
+        connection.Open();
+        MySqlDataReader reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Movie movie = new()
+            {
+                Id = reader.GetInt32("id"),
+                Title = reader.GetString("title"),
+                OriginalTitle = reader.GetString("originalTitle"),
+                Synopsis = reader.GetString("synopsis"),
+                MovieYear = reader.GetInt16("movieYear"),
+                Duration = reader.GetInt16("duration"),
+                AgeRating = reader.GetByte("ageRating"),
+                Image = reader.GetString("image")
+            };
+            movies.Add(movie);
+        }
+        connection.Close();
+        return movies;
+    }
+
+    public Movie ReadById(int? id)
+    {
+        MySqlConnection connection = new(connectionString);
+        string sql = "select * from Movie where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", id);
+        
+        connection.Open();
+        MySqlDataReader reader = command.ExecuteReader();
+        reader.Read();
+        if (reader.HasRows)
+        {
+            Movie movie = new()
+            {
+                Id = reader.GetInt32("id"),
+                Title = reader.GetString("title"),
+                OriginalTitle = reader.GetString("originalTitle"),
+                Synopsis = reader.GetString("synopsis"),
+                MovieYear = reader.GetInt16("movieYear"),
+                Duration = reader.GetInt16("duration"),
+                AgeRating = reader.GetByte("ageRating"),
+                Image = reader.GetString("image")
+            };
+            connection.Close();
+            return movie;
+        }
+        connection.Close();
+        return null;
+    }
+
+    public void Update(Movie model)
+    {
+        MySqlConnection connection = new(connectionString);
+        string sql = "update Movie set "
+                        + "Title = @Title, "
+                        + "OriginalTitle = @OriginalTitle, "
+                        + "Synopsis = @Synopsis, "
+                        + "MovieYear = @MovieYear, "
+                        + "Duration = @Duration, "
+                        + "AgeRating = @AgeRating, "
+                        + "Image = @Image "
+                    + "where Id = @Id";
+        MySqlCommand command = new(sql, connection)
+        {
+            CommandType = CommandType.Text
+        };
+        command.Parameters.AddWithValue("@Id", model.Id);
+        command.Parameters.AddWithValue("@Title", model.Title);
+        command.Parameters.AddWithValue("@OriginalTitle", model.OriginalTitle);
+        command.Parameters.AddWithValue("@Synopsis", model.Synopsis);
+        command.Parameters.AddWithValue("@MovieYear", model.MovieYear);
+        command.Parameters.AddWithValue("@Duration", model.Duration);
+        command.Parameters.AddWithValue("@AgeRating", model.AgeRating);
+        command.Parameters.AddWithValue("@Image", model.Image);
+        
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
+
+
